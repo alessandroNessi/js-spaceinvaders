47056e5cee88df9486d42f24fd905b5ee9745239
alessandroNessi <alessandronessi@msn.com>
2021-09-10 17:41:06 +0200

    fixed a bug where the bullets get stuck

--- script/script.js
+++ script/script.js
@@ -9,8 +9,11 @@ let x=0, y=0, count=0, score=0, difficoult=100, autofire=false;
 //starship
 function starShip(x,y){
     this.x=x;
+    let xPrec="start";
     this.y=y;
+    let yPrec="start";
     this.kind="starShip";
+
 }
 //bullet
 function bullet(x,y){
@@ -41,16 +44,17 @@ const gamePlay= () => {
         bullets.push(new bullet(spaceShip.x,spaceShip.y-1));
     }
     for(let i=0;i<bullets.length;i++){
-        drawcell(bullets[i].x,bullets[i].y,bullets[i].kind);
+        drawcell(bullets[i].x,bullets[i].y,bullets[i].kind,"remove");
         if(bullets[i].y==spaceShip.y-2){
-            drawcell(bullets[i].x,bullets[i].y+1,bullets[i].kind);
+            drawcell(bullets[i].x,bullets[i].y+1,bullets[i].kind,"remove");
         }
         if(bullets[i].y!=0){
             bullets[i].y-=1;
-            drawcell(bullets[i].x,bullets[i].y,bullets[i].kind);
+            drawcell(bullets[i].x,bullets[i].y,bullets[i].kind,"add");
             for(let t=0;t<enemies.length;t++){
                 if(bullets[i].x==enemies[t].x&&bullets[i].y==enemies[t].y){
-                    drawcell(bullets[i].x,bullets[i].y,);
+                    drawcell(bullets[i].x,bullets[i].y,bullets[i].kind,"remove");
+                    drawcell(enemies[t].x,enemies[t].y,enemies[t].kind,"remove");
                     enemies.splice(t,1);
                     bullets.splice(i,1);
                     score++;
@@ -71,34 +75,34 @@ const gamePlay= () => {
     if(count%5==0 && count%10!=0){
         if(enemies[0].x%2==0){
             for(let i=0;i<enemies.length;i++){
-                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind);
+                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind,"remove");
                 enemies[i].x+=1;
                 if(enemies[i].y==20){
                     endGame();
                 }
-                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind);
+                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind,"add");
                 wave=true;
             }
         }else{
             for(let i=0;i<enemies.length;i++){
-                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind);
+                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind,"remove");
                 enemies[i].x-=1;
                 if(enemies[i].y==20){
                     endGame();
                 }
-                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind);
+                drawcell(enemies[i].x,enemies[i].y,enemies[i].kind,"add");
             }
             wave=false;
         }
     }
     if(count%10==0){
         for(let i=0;i<enemies.length;i++){
-            drawcell(enemies[i].x,enemies[i].y,enemies[i].kind);
+            drawcell(enemies[i].x,enemies[i].y,enemies[i].kind,"remove");
             enemies[i].y+=1;
             if(enemies[i].y==20){
                 endGame();
             }
-            drawcell(enemies[i].x,enemies[i].y,enemies[i].kind);
+            drawcell(enemies[i].x,enemies[i].y,enemies[i].kind,"add");
         }
     }
     count++;
@@ -120,10 +124,10 @@ const loadGameplay = ()=>{
             y+=2;
         }
         enemies.push(new enemy(x,y,"left"));
-        drawcell(enemies[i].x,enemies[i].y,enemies[i].kind);
+        drawcell(enemies[i].x,enemies[i].y,enemies[i].kind,"add");
         x+=2
     }
-    drawcell(spaceShip.x,spaceShip.y,spaceShip.kind);
+    drawcell(spaceShip.x,spaceShip.y,spaceShip.kind,"add");
     t = setInterval(function(){
         gamePlay();
     }, difficoult);
@@ -146,17 +150,35 @@ const reloadGameplay = ()=>{
 }
 
 /**a function that draw the ship in a cell based on it's kind */
-const drawcell = (x,y,kind)=>{
+const drawcell = (x,y,kind,addRemove)=>{
     let cell=y*20+x;
     switch (kind){
         case "starShip":
-            document.getElementsByClassName("cell")[cell].classList.toggle("starship");
+            if(addRemove=="remove"){
+            document.getElementsByClassName("cell")[cell].classList.value="cell";
+            // document.getElementsByClassName("cell")[cell].classList.remove("starship");
+            }else if(addRemove=="add"){
+                document.getElementsByClassName("cell")[cell].classList.add("starship");
+            }
             break;
         case "bullet":
-            document.getElementsByClassName("cell")[cell].classList.toggle("bullet");
+            if(addRemove=="remove"){
+            document.getElementsByClassName("cell")[cell].classList.value="cell";
+            // document.getElementsByClassName("cell")[cell].classList.remove("bullet");
+            }else if(addRemove=="add"){
+                document.getElementsByClassName("cell")[cell].classList.add("bullet");
+            }
             break;
+            // document.getElementsByClassName("cell")[cell].classList.toggle("bullet");
+            // break;
         case "enemy":
-            document.getElementsByClassName("cell")[cell].classList.toggle("enemy");
+            // document.getElementsByClassName("cell")[cell].classList.toggle("enemy");
+            if(addRemove=="remove"){
+            document.getElementsByClassName("cell")[cell].classList.value="cell";
+            // document.getElementsByClassName("cell")[cell].classList.remove("enemy");
+            }else if(addRemove=="add"){
+                document.getElementsByClassName("cell")[cell].classList.add("enemy");
+            }
             break;
         default:
             document.getElementsByClassName("cell")[cell].classList.value="cell";
@@ -170,21 +192,21 @@ document.addEventListener("keydown",function(event){
     switch (event.code) {
         case 'ArrowLeft'://if i press arrowleft
             if(spaceShip.x>0){
-                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind);//delete last spaceship position
+                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind,"remove");//delete last spaceship position
                 spaceShip.x-=1;
-                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind);//draw new spaceship position
+                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind,"add");//draw new spaceship position
             }
             break;  
         case 'ArrowRight':
             if(spaceShip.x<19){
-                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind);//delete last spaceship position
+                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind,"remove");//delete last spaceship position
                 spaceShip.x+=1;
-                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind);//draw last spaceship position
+                drawcell(spaceShip.x,spaceShip.y,spaceShip.kind,"add");//draw new spaceship position
             }
             break;
         case 'Space':
             if(autofire==false){
-                bullets.push(new bullet(spaceShip.x,spaceShip.y-1));//push an object bullet in the bullets array
+                bullets.push(new bullet(spaceShip.x,spaceShip.y-1,"add"));//push an object bullet in the bullets array
             }
             break;
         case 'KeyF':
@@ -203,9 +225,7 @@ document.getElementById("start").addEventListener("click", function(){
         load();
         loadGameplay();
         var audio = new Audio('media/Doom_OST.mp3');
-        audio.play();
+        // audio.play();
         gameStart=true;//i don't let the button be clicked multiple times because if i don't it would start a new game pressing space
     }
 });
-
-
